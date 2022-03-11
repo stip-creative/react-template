9eb4d92f82481059a5946d143cd7630093ad4d9f
Ilya Stip <ilyastipakov83@gmail.com>
2022-03-11 18:03:18 +0200

    add useCustomScroll

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, PropsWithChildren } from "react";
 
+// import useCustomScroll from "../../hooks/useCustomScroll";
+
 import StyledPageWrapper from "./style";
 
 const Page: FunctionComponent<PropsWithChildren<Record<never, never>>> = ({ children }) => {
