5029e79a7b3bc74a30e5721bbd139efc49c0b281
Ilya Stip <ilyastipakov83@gmail.com>
2022-09-17 11:18:23 +0300

    add gitignore

---
+++ webpack.client.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+const Dotenv = require("dotenv-webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+const webpackConfig = require("./webpack.config");
+
+module.exports = (env, argv) => {
+    const buildFolderName = "build";
+    const buildPath = path.resolve(__dirname, buildFolderName);
+
+    const watchMode = argv.liveReload || false;
+    const modeEnv = argv.mode || "development";
+    const config = webpackConfig(modeEnv);
+
+    return {
+        mode: modeEnv,
+        entry: ["@babel/polyfill", "./src/index.tsx"],
+        output: {
+            filename: watchMode ? "assets/[name].[hash].js" : "assets/[name].[chunkhash].js",
+            path: buildPath,
+            publicPath: "./",
+            clean: true,
+        },
+        module: {
+            rules: [
+                config.modules.js,
+                config.modules.style,
+                config.modules.glsl,
+                config.modules.files,
+                config.modules.fonts,
+                config.modules.graphql,
+                config.modules.svg,
+                config.modules.gltf,
+            ],
+        },
+        resolve: config.resolve,
+        plugins: [
+            ...config.plugins,
+            new CompressionPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+            }),
+            new webpack.ProvidePlugin({
+                process: "process/browser",
+            }),
+            new WebpackNotifierPlugin({ alwaysNotify: false }),
+            new Dotenv({
+                systemvars: true,
+            }),
+        ],
+        devServer: {
+            devMiddleware: {
+                writeToDisk: true,
+            },
+            client: {
+                overlay: false,
+            },
+            static: {
+                directory: buildPath,
+            },
+            port: 3000,
+            open: true,
+            historyApiFallback: true,
+        },
+    };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,92 @@
 const path = require("path");
-const ReactLoadablePlugin =
-    require("react-loadable/webpack").ReactLoadablePlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
-module.exports = {
-    entry: {
-        client: "./src/client.js",
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        publicPath: "/",
-        chunkFilename: "[name].bundle.js",
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-        ],
-    },
-    plugins: [
-        new ReactLoadablePlugin({
-            filename: "./public/react-loadable.json",
+module.exports = (env) => {
+    const modules = {
+        js: {
+            test: /\.(ts|js)x?$/i,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+            },
+        },
+        style: {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                {
+                    loader: "style-loader",
+                },
+                {
+                    loader: "css-loader",
+                },
+                {
+                    loader: "sass-loader",
+                },
+            ],
+        },
+        glsl: {
+            test: /\.glsl$/,
+            exclude: /node_modules/,
+            use: ["raw-loader", "glslify-loader"],
+        },
+        files: {
+            test: /\.(png|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+        svg: {
+            test: /\.svg$/,
+            use: ["@svgr/webpack", "url-loader"],
+        },
+        fonts: {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+        },
+        graphql: {
+            test: /\.(graphql|gql)$/,
+            exclude: /node_modules/,
+            loader: "graphql-tag/loader",
+        },
+        gltf: {
+            test: /\.(glb|gltf)$/,
+            use: [
+                {
+                    loader: "file-loader",
+                    // options: {
+                    //  outputPath: 'assets/models'
+                    // }
+                },
+            ],
+        },
+    };
+
+    const plugins = [
+        new ESLintPlugin({
+            files: "src/**/*{ts,tsx}",
+            failOnError: false,
         }),
-    ],
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+    ];
+
+    const resolve = {
+        extensions: [
+            ".ts",
+            ".tsx",
+            ".js",
+            ".jsx",
+            ".glsl",
+            ".jpg",
+            ".png",
+            ".svg",
+        ],
+    };
+
+    return {
+        modules,
+        plugins,
+        resolve,
+    };
 };
